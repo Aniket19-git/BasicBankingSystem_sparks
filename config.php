59290cb02b750fee2c712f38a68fa7c1b246f0d0
Aniket19-git <101111801+Aniket19-git@users.noreply.github.com>
2022-03-07 17:56:38 +0530

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $servername = "localhost";
-   $username = "root";
-   $password = '';
-   $dbname = "sparksbank"; 
-
-   $conn = mysqli_connect($servername, $username, $password, $dbname) or dir("unable to connect");
-?>
